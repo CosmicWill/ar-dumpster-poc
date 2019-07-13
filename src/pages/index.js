935decfb7a6f4ab352c38725299c5d19b0228e3f
CosmicWill <wilfredor@mail.usf.edu>
2019-07-13 16:37:20 -0500

    added model viewer

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import '@google/model-viewer';
 
 const IndexPage = () => (
   <Layout>
@@ -12,7 +13,8 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      <model-viewer src="https://poly.googleapis.com/downloads/fp/1563010914490043/5vbJ5vildOq/7vBfIqvpuoG/model.gltf" ar camera-controls alt="A 3D model of an astronaut" background-color="#222" ></model-viewer>
+
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
