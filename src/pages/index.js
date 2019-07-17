97de0eb6b620861b727a8c8368b5df225f05e331
Jami Tefridj <jamitefridj@gmail.com>
2019-07-17 11:13:01 -0500

    removing default image

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,6 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image/>
       <model-viewer src="models/container10.gltf" ar camera-controls alt="Model of Dumpster" background-color="#222" ios-src="models/container10.usdz" quick-look-browsers= "safari chrome"></model-viewer>
 
     </div>
