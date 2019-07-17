9167956fa6edd966a12a34e45c2c7e221044ee36
Jami Tefridj <jamitefridj@gmail.com>
2019-07-17 11:25:35 -0500

    updating model directory 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <model-viewer src="/models/container10.gltf" ar camera-controls alt="Model of Dumpster" background-color="#222" ios-src="/models/container10.usdz" quick-look-browsers= "safari chrome"></model-viewer>
+      <model-viewer src="/src/models/container10.gltf" ar camera-controls alt="Model of Dumpster" background-color="#222" ios-src="/src/models/container10.usdz" quick-look-browsers= "safari chrome"></model-viewer>
 
     </div>
     <Link to="/page-2/">Go to page 2</Link>
