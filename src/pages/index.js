57bb7b10cdff65ed9e8787c0d1107177278ae421
Jami Tefridj <jamitefridj@gmail.com>
2019-07-17 10:44:06 -0500

    added dumpster models, changed index for local models and ios chrome support

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,8 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <model-viewer src="https://poly.googleapis.com/downloads/fp/1563010914490043/5vbJ5vildOq/7vBfIqvpuoG/model.gltf" ar camera-controls alt="A 3D model of an astronaut" background-color="#222" ></model-viewer>
+      <Image/>
+      <model-viewer src="models/container10.gltf" ar camera-controls alt="Model of Dumpster" background-color="#222" ios-src="models/container10.usdz" quick-look-browsers= "safari chrome"></model-viewer>
 
     </div>
     <Link to="/page-2/">Go to page 2</Link>
