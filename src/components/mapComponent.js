65b7b2ae68edb7ee0516f1bef2b185d599460ad2
spencermaxwell <spencermaxwell96@gmail.com>
2019-07-25 08:26:58 -0700

    working with polyline

--- src/components/mapComponent.js
+++ src/components/mapComponent.js
@@ -6,7 +6,12 @@ function Map() {
         <GoogleMap
         defaultZoom={19}
         defaultCenter={{ lat: 34.024254, lng: -118.492986 }}
+        onClick={ console.log("click")}
+
+        
       >
+
+        
 
       <Marker key = "default" position = {{ lat: 34.024254, lng: -118.492986 }} />
       >
@@ -21,22 +26,45 @@ function Map() {
 
             }}
             />
+
         </GoogleMap>
     );
 }
 
-const WrappedMap = withScriptjs(withGoogleMap(Map)); 
+function HandleClick() {
+    console.log("clicked");
 
+    // var poly = new Polyline({
+    //     path= {[{lat: 34.0245, lng: -118.4928}, {lat: 34.024, lng: -118.493 }] }
+    //     geodesic={true}
+    //     options={{
+    //     strokeColor: "#ff2527",
+    //     strokeOpacity: 0.75,
+    //     strokeWeight: 2,
+
+    //     }}
+        
+    // });
+
+    // poly.mapElement(WrappedMap)
+};
+
+const WrappedMap = withScriptjs(withGoogleMap(Map)); 
   
 export default function MapComponent() {
+
     return (
         <div>
             <WrappedMap
             googleMapURL = {' https://maps.googleapis.com/maps/api/js?key=AIzaSyABZ-W1LP5XHasJEF0z3NMzRkkX5uP4_xA'}
             loadingElement={<div style={{ height: `100%` }} />}
             containerElement={<div style={{ height: `200px` }} />}
-             mapElement={<div style={{ height: `100%` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+            onClick={ console.log("click")}
+
+
             />
         </div>
+
     )
 }
